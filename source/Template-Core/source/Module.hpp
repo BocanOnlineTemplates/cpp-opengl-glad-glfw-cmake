f8d97fb3affa1fa20129a329acf2c6ca42de1526
buchananmatt <buch.matt@gmail.com>
2025-12-28 18:50:16 -0700

    (feat) initial commit

---
+++ source/Template-Core/source/Module.hpp
@@ -0,0 +1,19 @@
+//
+// ProjectTemplate
+// Bocan Online C++ Template Project
+// Module.hpp
+//
+#pragma once
+
+class Module {
+
+public:
+     Module();
+    ~Module();
+
+
+private:
+
+private:
+
+};
